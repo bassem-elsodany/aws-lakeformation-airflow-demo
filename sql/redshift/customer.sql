4cb1a7575662a14b5efbdec9cb47d3c06aa1548d
Bassem Elsodany <bassem.elsodany@gmail.com>
2022-03-15 13:07:35 +0000

    cleanup

--- sql/redshift/customer.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE "public"."customer"(id        integer NOT NULL encode az64,
-                                 firstname character varying(256) encode lzo,
-                                 lastname  character varying(256) encode lzo,
-                                 gender    character varying(256) encode lzo,
-                                 CONSTRAINT customer_pkey PRIMARY KEY(id)) distkey(id);
